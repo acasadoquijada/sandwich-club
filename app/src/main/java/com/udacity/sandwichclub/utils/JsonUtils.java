f31450a97ab665ceea5c64c15703d5b4f5210ab6
acasado <acasadoquijada@gmail.com>
2020-01-22 11:36:16 +0100

    Initial commit

---
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -0,0 +1,22 @@
+package com.udacity.sandwichclub.utils;
+
+import com.udacity.sandwichclub.model.Sandwich;
+
+public class JsonUtils {
+
+    // I need all of this:
+
+    /*
+    private String mainName;
+    private List<String> alsoKnownAs = null;
+    private String placeOfOrigin;
+    private String description;
+    private String image;
+    private List<String> ingredients = null;
+     */
+
+    // its in strings.xml
+    public static Sandwich parseSandwichJson(String json) {
+        return null;
+    }
+}
